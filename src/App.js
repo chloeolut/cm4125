def193d472feccd8c37e67c0cdd46b846afe7a3a
Chloe <1812144@rgu.ac.uk>
2022-04-10 00:15:00 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import love4uglies from './images/cuteduck.jpeg';
 
 const App = () => {
     return(
-                <Container>
-                   
-                   
-                    </Container>
+                <Container maxWidth="lg">
+                    <AppBar position="static" color="inherit">
+                        <Typography variant="h2" align="center">LoveForUglies</Typography>
+                        <img src ={love4uglies} alt="love4uglies" height="60"/>
+
+                    </AppBar>
+               </Container>
     );
 }
 
