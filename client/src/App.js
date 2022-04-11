f2a7e888f51d691e8ef7c56bc62c28a2f4b75966
Chloe <1812144@rgu.ac.uk>
2022-04-11 14:46:04 +0100

    Latest commits so far

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import love4uglies from './images/cuteduck.jpeg';
+import { getPosts } from './actions/posts';
 import useStyles from './styles';
+import love4uglies from './images/cuteduck.jpeg';
+
 
 const App = () => {
-    const classes = useStyles();
-    const dispatch =  useDispatch();
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-    useEffect(() => {
-            dispatch(getPosts);
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
 
-    return(
-                <Container maxWidth="lg">
-                    <AppBar className={classes.appBar} position="static" color="inherit">
-                        <Typography className={classes.heading} variant="h2" align="center">LoveForUglies</Typography>
-                        <img className = {classes.image} src ={love4uglies} alt="love4uglies" height="60"/>
-
-                    </AppBar>
-                    <Grow in>
-                        <Container>
-                            <Grid container justify="space-between" alignItems="stretch" spacing = {3}>
-                                 
-                            <Grid item xs={12} sm={7}>
-                                    <Posts /> 
-                            </Grid>
-                            <Grid item xs={12} sm={7}>
-                                        <Form />
-                            
-                            </Grid>
-
-                            </Grid>
-                        </Container>
-
-                    </Grow>
-               </Container>
-    );
-}
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Love For Uglies</Typography>
+        <img className={classes.image} src={love4uglies} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
 
